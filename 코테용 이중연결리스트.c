5ee5b6c8725a52a8c2b018d23bf40b82ac00b15d
wjdtngh950 <wjdtngh950@naver.com>
2021-07-05 18:48:31 +0900

    Create 코테용 이중연결리스트.c

---
+++ 코테용 이중연결리스트.c
@@ -0,0 +1,45 @@
+//
+// Created by 정수호 on 05/07/2021.
+//
+
+#include <stdio.h>
+
+typedef struct _Node {
+    int val;
+    struct _Node* prev;
+    struct _Node* next;
+}Node;
+
+Node buf[11000000];
+int bufCnt;
+Node* head;
+Node* last;
+
+Node* myAlloc(int n) {
+    buf[bufCnt].val = n;
+    buf[bufCnt].prev = last;
+    buf[bufCnt].next = NULL;
+    return &buf[bufCnt++];
+}
+
+void addNode(int value) {
+    if (head == NULL) {
+        last = head = myAlloc(value);
+    }
+    else {
+        last = last->next = myAlloc(value);
+    }
+}
+
+int main(void) {
+    addNode(1);
+    addNode(2);
+    addNode(3);
+    addNode(4);
+    addNode(5);
+
+    for (Node* p = head; p != NULL; p = p->next) {
+        printf("%d -> ", p->val);
+    }
+    return 0;
+}
