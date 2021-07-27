78a6765dca6da81638d7b459dc28840206b2dcc8
wjdtngh950 <wjdtngh950@naver.com>
2021-07-27 16:16:56 +0900

    Update baek_list_key.c

--- baek_list_key.c
+++ baek_list_key.c
@@ -7,78 +7,89 @@ typedef struct _Node {
     struct _Node* next;
 } Node;
 
-Node* head = NULL;
-Node* last = NULL;
-Node* cursor = NULL;
+Node* head;
+Node* last;
+Node* cursor;
 
-void addNode(char value) { //커서의 왼쪽에 삽
-    if (cursor == head) {
-        head = (Node*)malloc(sizeof(Node));
+void addNode(char value) { //뒤에 삽입해줌
+    if (head == NULL) {
+        last = head = (Node*)malloc(sizeof(Node));
         head->val = value;
         head->prev = NULL;
-        head->next = cursor;
-        cursor->prev = head;
+        head->next = NULL;
     }
     else {
         Node* newNode = (Node*)malloc(sizeof(Node));
         newNode->val = value;
-        newNode->prev = cursor->prev;
-        newNode->next = cursor;
-        cursor->prev->next = newNode;
-        cursor->prev = newNode;
+        newNode->prev = last;
+        newNode->next = NULL;
+        last = last->next = newNode;
+        // addNode는 연결리스트의 맨 뒤에 삽입하는 함수
+        // newNode는 연결리스트의 맨 뒤에 삽입될 노드
+        // last->next는 기존에 NULL 값을 가지고 있는데,
+        // 새로 노드가 삽입되므로, 새로 생성한 노드의 주소를
+        // 기존에 last 포인터가 가리키는 노드의 next 링크에 할당함
+        // last 포인터도 새로 생성한 노드를 가리키도록 변경
+        // last->next = newNode;
+        // last = newNode;
     }
 }
 
-void printList() {
+int main(void) {
+    char buf[110000];
+    scanf("%s", buf);
+    for (int i = 0; buf[i] != '\0'; i++) {
+        addNode(buf[i]);
+    }
+    addNode('$');
+    cursor = last;
+    int M;
+    scanf("%d", &M);
+    for (int m = 0; m < M; m++) {
+        char cmd;
+        scanf(" %c", &cmd);
+        if (cmd == 'L') {
+            if (cursor != head) {
+                cursor = cursor->prev;
+            }
+        }
+        else if (cmd == 'D') {
+            if (cursor != last) {
+                cursor = cursor->next;
+            }
+        }
+        else if (cmd == 'B') {
+            if (cursor != head) {
+                if (cursor->prev != head) {
+                    cursor->prev->prev->next = cursor;
+                    cursor->prev = cursor->prev->prev;
+                }
+                else {
+                    cursor->prev = head;
+                    head = cursor;
+                }
+            }
+        }
+        else {
+            char c;
+            scanf(" %c", &c);
+            Node* newNode = (Node*)malloc(sizeof(Node));
+            newNode->val = c;
+            newNode->prev = cursor->prev;// ??newNode->prev = NULL이 왜 아닌지??
+            newNode->next = cursor;
+            if (cursor == head) {
+                head = newNode;
+            }
+            else {
+                cursor->prev->next = newNode;
+            }
+            cursor->prev = newNode;
+        }
+    }
     for (Node* p = head; p->val != '$'; p = p->next) {
         printf("%c", p->val);
     }
     printf("\n");
-}
-
-int main(void) {
-    int M;
-    scanf("%d", &M);
-    for (int m = 0; m < M; m++) {
-        char buf[1000000];
-        scanf("%s", buf);
-        cursor = (Node*)malloc(sizeof(Node));
-        cursor->val = '$';
-        cursor->prev = NULL;
-        cursor->next = NULL; // 새로운 노드 '$' 생성 & 초기화
-        head = last = cursor; // 비어있는 연결리스트에 노드 삽입
-        for (int i = 0; buf[i] != '\0'; i++) {
-            if (buf[i] == '<') {
-                if (cursor != head) {
-                    cursor = cursor->prev;
-                }
-            }
-            else if (buf[i] == '>') {
-                if (cursor != last) {
-                    cursor = cursor->next;
-                }
-            }
-            else if (buf[i] == '-') {
-                if (cursor!=head) { // 삭제할 수 있는 노드가 있는지 확인
-                    if (cursor->prev->prev == NULL) { // 삭제할 노드가 헤드
-                        head = cursor;
-                    }
-                    else {
-                        cursor->prev->prev->next = cursor;
-                    }
-                    cursor->prev = cursor->prev->prev;
-                    // 삭제할 노드가 헤드 노드인 경우에도
-                    // 삭제 이후 cursor가 가리키는 노드의
-                    // prev 링크를 NULL 값으로 설정해줘야 하기 때문에
-                    // if / else 구문 모두 적용해야 하는 코드(69 Line)
-                }
-            }
-            else {
-                addNode(buf[i]);
-            }
-        }
-        printList();
-    }
 
     return 0;
 }
