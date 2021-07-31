c109cd013c2d25415825588a595b687adf6d89b3
wjdtngh950 <wjdtngh950@naver.com>
2021-08-01 08:09:41 +0900

    Create practice.c

---
+++ practice.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+
+#define TRUE 1
+#define FALSE 0
+#define STACK_SIZE 110000
+
+int stack[STACK_SIZE];
+int top=-1;
+
+int is_full(){
+    if(top==STACK_SIZE-1){
+        return TRUE;
+    }
+    return FALSE;
+}
+
+int is_empty(){
+    if(top==-1){
+        return TRUE;
+    }
+    return FALSE;
+}
+
+void push(int val){
+    if(is_full()){
+        return;
+    }
+    top++;
+    stack[top]=val;
+}
+
+int pop(){
+    if(is_empty()){
+        return '\0';
+    }
+    int rtn=stack[top];
+    top--;
+    return rtn;
+    //return stack[top--];
+}
+
+int main(){
+    int N;
+    int sum=0;
+    scanf("%d", &N);
+
+    for(int n=0;n<N;n++){
+        int m;
+        scanf("%d", &m);
+        if(m==0){
+            pop();
+        }
+        else{
+            push(m);
+        }
+    }
+
+    return 0;
+}
