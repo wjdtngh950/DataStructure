38024ee01cd6c6e18fa1b40e4c58fb6c40b6220a
wjdtngh950 <wjdtngh950@naver.com>
2021-08-01 22:27:47 +0900

    Create baek_stack_10828-2.c

---
+++ baek_stack_10828-2.c
@@ -0,0 +1,70 @@
+//
+// Created by 정수호 on 01/08/2021.
+//
+
+#include<stdio.h>
+
+#define TRUE 1
+#define FALSE 0
+#define STACK_SIZE 1100000
+
+int stack[STACK_SIZE];
+int top = -1;
+
+int is_full() {
+    if (top == STACK_SIZE - 1) {
+        return TRUE;
+    }
+    return FALSE;
+}
+
+int is_empty() {
+    if (top == -1) {
+        return TRUE;
+    }
+    return FALSE;
+}
+
+void push(int val) {
+    if (is_full()) {
+        return;
+    }
+    top++;
+    stack[top] = val;
+}
+
+int pop() {
+    if (is_empty()) {
+        return -1;
+    }
+    int rtn = stack[top];
+    top--;
+    return rtn;
+}
+
+int main() {
+    int N;
+    scanf("%d", &N);
+    for (int i = 0; i < N; i++) {
+        char buf[6];
+        scanf("%s", buf);
+        if (buf[0] == 'p' && buf[1] == 'u') {
+            int X;
+            scanf("%d", &X);
+            push(X);
+        } else if (buf[0] == 'p' && buf[1] == 'o') {
+            printf("%d\n", pop());
+        } else if (buf[0] == 's') {
+            printf("%d\n", top + 1);
+        } else if (buf[0] == 'e') {
+            printf("%d\n", is_empty());
+        } else if(buf[0]=='t'){
+            if (!(is_empty())) {
+                printf("%d\n", stack[top]);
+            } else {
+                printf("-1\n");
+            }
+        }
+    }
+    return 0;
+}
