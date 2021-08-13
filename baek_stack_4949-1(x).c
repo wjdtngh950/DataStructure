742771c608d10fd206491d7236b4d1cff06d928b
wjdtngh950 <wjdtngh950@naver.com>
2021-08-13 17:10:18 +0900

    Update baek_stack_4949-1(x).c

--- baek_stack_4949-1(x).c
+++ baek_stack_4949-1(x).c
@@ -7,69 +7,74 @@
 char stack[STACK_SIZE];
 int top=-1;
 
-int is_full() {
-    if (top == STACK_SIZE - 1) {
+int is_full(){
+    if(top==STACK_SIZE-1){
         return TRUE;
     }
     return FALSE;
 }
 
-int is_empty() {
-    if (top == -1) {
+int is_empty(){
+    if(top==-1){
         return TRUE;
     }
     return FALSE;
 }
 
-void push(char val) {
-    if (is_full()) {
+void push(char val){
+    if(is_full()){
         return;
     }
     top++;
-    stack[top] = val;
+    stack[top]=val;
 }
 
-char pop() {
-    if (is_empty()) {
+char pop(){
+    if(is_empty()){
         return '\0';
     }
-    char rtn = stack[top];
+    char rtn=stack[top];
     top--;
     return rtn;
 }
 
-int main() {
-    while (1) {
+int main(){
+    while(1){
         char buf[101];
-        scanf("%s", buf);
         int error=FALSE;
-        int cnt1=0;
-        int cnt2=0;
         top=-1;
-        for (int i = 0; buf[i] != '.'; i++) {
-            if (buf[i] == '(') {
+        scanf("%s", buf);
+        if(buf[0]=='.'&&buf[1]=='\0'){
+            break;
+        }
+        for(int i=0;buf[i]!='.';i++){
+            if(buf[i]=='('){
                 push(buf[i]);
-                cnt1++;
             }
             else if(buf[i]==')'){
-                if(is_empty()||cnt2!=0){
+                if(is_empty()){
+                    error=TRUE;
+                    break;
+                }
+                else if(stack[top]!='('){
                     error=TRUE;
                     break;
                 }
                 pop();
-                cnt1--;
             }
             else if(buf[i]=='['){
                 push(buf[i]);
-                cnt2++;
             }
             else if(buf[i]==']'){
-                if(is_empty()||cnt1!=0){
+                if(is_empty()){
+                    error=TRUE;
+                    break;
+                }
+                else if(stack[top]!='['){
                     error=TRUE;
                     break;
                 }
                 pop();
-                cnt2--;
             }
         }
         if(!(is_empty())){
@@ -78,12 +83,9 @@ int main() {
         if(error==TRUE){
             printf("NO\n");
         }
-        else{
+        else {
             printf("YES\n");
         }
-
-        //break; //나중에 지워주기
     }
-
     return 0;
 }
