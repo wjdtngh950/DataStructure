d85464752f649037bfc9b667dfc7a2d6ff8d4ead
wjdtngh950 <wjdtngh950@naver.com>
2021-08-22 13:50:31 +0900

    Create baek_queue_18258.c

---
+++ baek_queue_18258.c
@@ -0,0 +1,95 @@
+#include<stdio.h>
+
+#define TRUE 1
+#define FALSE 0
+#define QUEUE_SIZE 2000001
+
+int queue[QUEUE_SIZE];
+int front =-1;
+int rear=-1;
+
+int is_full(){
+    if(rear==QUEUE_SIZE-1){
+        return TRUE;
+    }
+    return FALSE;
+}
+
+int is_empty(){
+    if(front==rear){
+        return TRUE;
+    }
+    return FALSE;
+}
+
+void enqueue(int val){
+    if(is_full()){
+        return;
+    }
+    rear++;
+    queue[rear]=val;
+}
+
+int dequeue(){
+    if(is_empty()){
+        return -1;
+    }
+    front++;
+    int rtn=queue[front];
+    return rtn;
+}
+
+int main(){
+    int N;
+    scanf("%d", &N);
+    for(int n=0;n<N;n++){
+        char buf[6];
+        scanf("%s", buf);
+        if(buf[0]=='p'&&buf[1]=='u'){
+            int X;
+            scanf("%d", &X);
+            enqueue(X);
+        }
+        else if(buf[0]=='p'){
+            if(is_empty()){
+                printf("-1\n");
+            }
+            else{
+                printf("%d\n", dequeue());
+            }
+        }
+        else if(buf[0]=='s'){
+            if(is_empty()){
+                printf("0\n");
+            }
+            else {
+                printf("%d\n", rear-front);
+            }
+        }
+        else if(buf[0]=='e'){
+            if(is_empty()){
+                printf("%d\n", TRUE);
+            }
+            else{
+                printf("%d\n", FALSE);
+            }
+        }
+        else if(buf[0]=='f'){
+            if(is_empty()){
+                printf("-1\n");
+            }
+            else {
+                printf("%d\n", queue[front + 1]);
+            }
+        }
+        else{
+            if(is_empty()){
+                printf("-1\n");
+            }
+            else{
+                printf("%d\n", queue[rear]);
+            }
+        }
+    }
+    return 0;
+}
