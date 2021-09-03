3e3d0ab33891152127e0ef0a9dc789bcc790f9eb
wjdtngh950 <wjdtngh950@naver.com>
2021-09-04 01:09:39 +0900

    Update practice.c

--- practice.c
+++ practice.c
@@ -1,63 +1,118 @@
-#include<stdio.h>
+#include "stdio.h"
 
 #define TRUE 1
 #define FALSE 0
-#define STACK_SIZE 1100000
+#define SIZE 1100000
 
-int stack[STACK_SIZE];
-int top=-1;
+int top = -1;
+int front = -1;
+int rear = -1;
+int stack[SIZE];
+int queue[SIZE];
 
-int is_full(){
-    if(top==STACK_SIZE-1){
+int DFS_full() {
+    if (top == SIZE - 1) {
         return TRUE;
     }
     return FALSE;
 }
 
-int is_empty(){
-    if(top==-1){
+int BFS_full() {
+    if (rear == SIZE - 1) {
         return TRUE;
     }
     return FALSE;
 }
 
-void push(int val){
-    if(is_full()){
+int DFS_empty() {
+    if (top == -1) {
+        return TRUE;
+    }
+    return FALSE;
+}
+
+int BFS_empty() {
+    if (front == rear) {
+        return TRUE;
+    }
+    return FALSE;
+}
+
+void push(int val1) {
+    if (DFS_full()) {
         return;
     }
-    top++;
-    stack[top]=val;
+    stack[++top] = val1;
 }
 
-int pop(){
-    if(is_empty()){
+void enqueue(int val2) {
+    if (BFS_full()) {
+        return;
+    }
+    queue[++rear] = val2;
+}
+
+int pop() {
+    if (DFS_empty()) {
         return -1;
     }
-    int rtn= stack[top];
-    top--;
-    return rtn;
-    //return stack[top--];
+    return stack[top--];
 }
 
-int main(){
-    int N;
-    scanf("%d", &N);
-    int sum=0;
-    for(int n=0;n<N;n++){
-        int m;
-        scanf("%d", &m);
-        if(m==0){
-            pop();
-        }
-        else{
-            push(m);
+int dequeue() {
+    if (BFS_full()) {
+        return -1;
+    }
+    return queue[++front];
+}
+
+int visitDFS[1001];
+int visitBFS[1001];
+int edge[1001][1001];
+
+int main() {
+    int N, M, V;
+    scanf("%d %d %d", &N, &M, &V);
+    for (int i = 0; i < M; i++) {
+        int v1, v2;
+        scanf("%d %d", &v1, &v2);
+        edge[v1][v2] = TRUE;
+        edge[v2][v1] = TRUE;
+    }
+    //DFS
+    push(V);
+    while (!(DFS_empty())){
+        int v=pop();
+        if(!(visitDFS[v])){
+            visitDFS[v]=TRUE;
+            printf("%d ", v);
+            for(int w=N;w>0;w--){
+                if(!(visitDFS[w])){
+                    if(edge[v][w]==TRUE){
+                        push(w);
+                    }
+                }
+            }
         }
     }
-    for(int i=0; i<=top;i++){
-        sum+=stack[i];
+    printf("\n");
+    //BFS
+    enqueue(V);
+    while (!(BFS_empty())){
+        int v=dequeue();
+        if(!(visitBFS[v])){
+            visitBFS[v]=TRUE;
+            printf("%d ", v);
+            for(int w=1;w<=N;w++){
+                if(!visitBFS[w]){
+                    if(edge[v][w]==TRUE){
+                        enqueue(w);
+                    }
+                }
+            }
+        }
     }
-    printf("%d", sum);
+
+
     return 0;
-
 }
-
