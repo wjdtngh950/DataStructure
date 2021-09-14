2ac95ad458c485b98fd684bdcfb48209bae7c5f9
wjdtngh950 <wjdtngh950@naver.com>
2021-09-14 23:01:23 +0900

    Update practice.c

---
+++ practice.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+
+int edge[1001][1001];
+int queue[5500000][3];
+int front = -1;
+int rear = -1;
+
+int N, M;
+int n_tomato;
+
+int dx[4] = {-1, 1, 0, 0};
+int dy[4] = {0, 0, -1, 1};
+
+void enqueue(int x, int y, int d) {
+    rear++;
+    queue[rear][0] = x;
+    queue[rear][1] = y;
+    queue[rear][2] = d;
+}
+
+void dequeue(int* x, int* y, int* d) {
+    front++;
+    *x = queue[front][0];
+    *y = queue[front][1];
+    *d = queue[front][2];
+}
+
+void get_input() {
+    scanf("%d %d", &N, &M);
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < N; j++) {
+            scanf("%d", &edge[i][j]);
+            if (edge[i][j] == 0) {
+                n_tomato++;
+            }
+            else if(edge[i][j] == 1){
+                n_tomato++;
+                enqueue(i, j, 0); // i, j 위치의 토마토는 0일에 익는다.
+                edge[i][j] = 0;
+            }
+        }
+    }
+}
+
+int bfs() {
+    int answer = 0;
+    while (front != rear) {
+        int cx, cy, cd;
+        dequeue(&cx, &cy, &cd);
+        if (edge[cx][cy] == 0) { // arr[curx][cury]에 방문하지 않았으면,
+            edge[cx][cy] = 1; // 방문처리
+            answer = cd;
+            n_tomato--;
+            for (int k = 0; k < 4; k++) {
+                int nx = cx + dx[k];
+                int ny = cy + dy[k];
+                if (0 <= nx && nx < M && 0 <= ny && ny < N) { // 배열 내부 체크
+                    if (edge[nx][ny] == 0) {
+                        enqueue(nx, ny, cd + 1);
+                    }
+                }
+            }
+        }
+    }
+    if (n_tomato == 0)
+        return answer;
+    else
+        return -1;
+}
+
+
+int main(void) {
+    get_input();
+    printf("%d", bfs());
+    return 0;
+}
