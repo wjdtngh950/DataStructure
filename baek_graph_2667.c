cd308fa091b4136091f37eb9f62b795f46a9021a
wjdtngh950 <wjdtngh950@naver.com>
2021-09-29 18:03:25 +0900

    Update baek_graph_2667.c

--- baek_graph_2667.c
+++ baek_graph_2667.c
@@ -17,33 +17,33 @@ int top = -1;
 int dy[] = {-1, 0, 1, 0};
 int dx[] = {0, 1, 0, -1};
 
-void push(int y, int x) {
+void push(int x, int y) {
     top++;
-    stack[top][0] = y;
-    stack[top][1] = x;
+    stack[top][0] = x;
+    stack[top][1] = y;
 }
 
-void pop(int* py, int* px) {
-    *py = stack[top][0];
-    *px = stack[top][1];
+void pop(int* x, int* y) {
+    *x = stack[top][0];
+    *y = stack[top][1];
     top--;
 }
 
 void dfs() {
     int cnt = 0; // 단지 내 집의 수
     while (top != -1) {
-        int cur_y, cur_x;
-        pop(&cur_y, &cur_x);
-        if (visit[cur_y][cur_x]==0) {
-            visit[cur_y][cur_x] = TRUE;
+        int cx, cy;
+        pop(&cx, &cy);
+        if (visit[cx][cy]==0) {
+            visit[cx][cy] = TRUE;
             cnt++;
             for (int k = 0; k < 4; k++) {
-                int next_y = cur_y + dy[k];
-                int next_x = cur_x + dx[k];
-                if (0 <= next_y && next_y < N && 0 <= next_x && next_x < N) {
-                    if (map[next_y][next_x] == 1) { //9/29 ->44-46항이 이해가안감 뭘뜻하는지..? 왜 ==0이아닌지?
-                        if (!visit[next_y][next_x]) {
-                            push(next_y, next_x);
+                int nx = cx + dx[k];
+                int ny = cy + dy[k];
+                if (0 <= nx && nx < N && 0 <= ny && ny < N) {
+                    if (map[nx][ny] == 1) { //9/29 ->44-46항이 이해가안감 뭘뜻하는지..? 왜 ==0이아닌지?
+                        if (!visit[nx][ny]) {
+                            push(nx, ny);
                         }
                     }
                 }
@@ -71,23 +71,23 @@ void bubble_sort() {
 
 int main(void) {
     scanf("%d", &N);
-    for (int y = 0; y < N; y++) {
-        for (int x = 0; x < N; x++) {
+    for (int x = 0; x < N; x++) {
+        for (int y = 0; y < N; y++) {
             // 입력이 0110100 처럼 들어오기 때문에
             // 정수형 변수에 입력을 받으면, 의도하지 않은 형태로 값이 입력됨
             // char형 변수에 입력을 받은 뒤, int로 변환하여 사용
             char c;
             scanf(" %c", &c);
-            map[y][x] = c - '0';
+            map[x][y] = c - '0';
         }
     }
 
-    for (int y = 0; y < N; y++) { //->9/29 85항부터가 무슨역할을하는지 ??
+    for (int x = 0; x < N; x++) { //->9/29 85항부터가 무슨역할을하는지 ??
                                   // 토마토같은경우는 get_input에서 바로 푸쉬를해줬는데 왜 73항 85항여긴 따로 ??
-        for (int x = 0; x < N; x++) {
-            if (map[y][x] == 1) { // 집
-                if (visit[y][x] == FALSE) {
-                    push(y, x);
+        for (int y = 0; y < N; y++) {
+            if (map[x][y] == 1) { // 집
+                if (visit[x][y] == FALSE) {
+                    push(x, y);
                     dfs();
                 }
             }
