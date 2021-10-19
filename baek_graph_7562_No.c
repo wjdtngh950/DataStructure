7b312004f4b43799fc3f61ae13f683998a57906b
wjdtngh950 <wjdtngh950@naver.com>
2021-10-19 23:20:39 +0900

    Update baek_graph_7562_No.c

--- baek_graph_7562_No.c
+++ baek_graph_7562_No.c
@@ -4,55 +4,50 @@
 #define FALSE 0
 #define SIZE 5500000
 
-int front =-1;
+int front = -1;
 int rear = -1;
-int queue[SIZE][2];
-int dx[4]={-1,1,0,0};
-int dy[4]={0,0,-1,1};
-int map[310][310];
+int queue[SIZE][3];
+int dx[8] = { -2, -1,  1,  2,  2,  1, -1, -2 };
+int dy[8] = {  1,  2,  2,  1, -1, -2, -2, -1 };
 int visit[310][310];
-int N,m,M,l,L;
-int knight=0;
+int T, N, m, M, l, L;
+int knight = 0;
 
-void enqueue(int x, int y){
+void enqueue(int x, int y, int d) {
     rear++;
-    queue[rear][0]=x;
-    queue[rear][1]=y;
+    queue[rear][0] = x;
+    queue[rear][1] = y;
+    queue[rear][2] = d;
 }
 
-void dequeue(int *x, int *y){
+void dequeue(int* x, int* y, int* d) {
     front++;
-    *x=queue[front][0];
-    *y=queue[front][1];
+    *x = queue[front][0];
+    *y = queue[front][1];
+    *d = queue[front][2];
 }
 
-void get_input(){
+void get_input() {
     scanf("%d", &N);
-    for(int i=0;i<N;i++){
-        for(int j=0;j<N;j++){
-            map[i][j]=0;
-        }
-    }
     scanf("%d %d", &m, &M);
-    scanf("%d %d", &l,&L);
-    map[m][M]=1;
-    map[l][L]=1;
+    scanf("%d %d", &l, &L);
+    enqueue(m, M, 0);
 }
-void bfs(){
-    while (front!=rear){
-        int cx, cy;
-        dequeue(&cx, &cy);
-        if(visit[cx][cy]==0){
-            visit[cx][cy]=1;
-            knight++;
-            for(int k=0;k<4;k++){
-                int nx=cx+dx[k];
-                int ny=cy+dy[k];
-                if(0<=nx&&nx<N&&0<=ny&&ny<N){
-                    if(map[nx][ny]==1){
-                        if(visit[nx][ny]==0){
-                            enqueue(nx,ny);
-                        }
+
+void bfs() {
+    while (front != rear) {
+        int cx, cy, cd;
+        dequeue(&cx, &cy, &cd);
+        if (visit[cx][cy] == 0) {
+            visit[cx][cy] = 1;
+            knight = cd;
+            if (cx == l && cy == L) break;
+            for (int k = 0; k < 8; k++) {
+                int nx = cx + dx[k];
+                int ny = cy + dy[k];
+                if (0 <= nx && nx < N && 0 <= ny && ny < N) {
+                    if (visit[nx][ny] == 0) {
+                        enqueue(nx, ny, cd + 1);
                     }
                 }
             }
@@ -60,9 +55,24 @@ void bfs(){
     }
 }
 
-int main(){
-    get_input();
+void init() {
+    front = rear = -1;
+    for (int i = 0; i < 300; i++) {
+        for (int j = 0; j < 300; j++) {
+            visit[i][j] = 0;
+        }
+    }
+}
 
+int main() {
+    scanf("%d", &T);
+
+    for (int tc = 0; tc < T; tc++) {
+        init();
+        get_input();
+        bfs();
+        printf("%d\n", knight);
+    }
 
     return 0;
 }
