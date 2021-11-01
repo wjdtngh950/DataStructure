e8a439b32f6b909f282de6b1cb8a6802c8121e53
wjdtngh950 <wjdtngh950@naver.com>
2021-11-01 16:18:15 +0900

    Update practice.c

--- practice.c
+++ practice.c
@@ -4,82 +4,71 @@
 #define FALSE 0
 #define SIZE 5500000
 
-int rear = -1;
+int top= -1;
 int front = -1;
-int queue[SIZE][3];
-int dx[4] = {-1, 1, 0, 0};
-int dy[4] = {0, 0, -1, 1};
-int box[1001][1001];
-int visit[1001][1001];
-int N,M;
-int tomato;
+int rear = -1;
+int stack[SIZE];
+int queue[SIZE];
 
-void enqueue(int x, int y, int d) {
-    rear++;
-    queue[rear][0] = x;
-    queue[rear][1] = y;
-    queue[rear][2] = d;
+int edge[1001][1001];
+int visit[1001];
+
+int N, M, K;
+
+void push(val1){
+    top++;
+    stack[top]=val1;
 }
 
-void dequeue(int *x, int *y, int *d) {
-    front++;
-    *x=queue[front][0];
-    *y=queue[front][1];
-    *d=queue[front][2];
+int pop(){
+    return stack[top--];
+}
+
+void enqueue(val2){
+    rear++;
+    queue[rear]=val2;
+}
+
+int dequeue(){
+   return queue[++front];
 }
 
 void get_input(){
-    scanf("%d %d", &M, &N);
-    for(int i =0; i<N;i++){
-        for(int j =0; j<M;j++){
-            int K;
-            scanf("%d", &K);
-            box[i][j]=K;
-            if(box[i][j]==0){
-                tomato++;
-            }
-            else if(box[i][j]==1){
-                tomato++;
-                enqueue(i,j,0);
-            }
-        }
-    }
+   scanf("%d %d %d", &N, &M, &K);
+   for(int i =0;i<M;i++){
+       int v1, v2;
+       scanf("%d %d", &v1, &v2);
+       edge[v1][v2]=TRUE;
+       edge[v2][v1]=TRUE;
+   }
 }
 
-int bfs() {
-    int answer=0;
-    while (front != rear) {
-        int cx, cy, cd;
-        dequeue(&cx, &cy, &cd);
-        if (box[cx][cy] != -1) {
-            if (visit[cx][cy] == FALSE) {
-                visit[cx][cy] = TRUE;
-                answer = cd;
-                tomato--;
-                for(int k=0;k<4;k++){
-                    int nx=cx+dx[k];
-                    int ny=cy+dy[k];
-                    if(0<=nx&&nx<N&&0<=ny&&ny<M){
-                        if(box[nx][ny]!=-1){
-                            if(visit[nx][ny]==FALSE){
-                                enqueue(nx,ny,cd+1);
-                            }
-                        }
+void dfs(){
+    push(K);
+    while (top!=-1){
+        int v=pop();
+        if(visit[v]==FALSE){
+            visit[v]=TRUE;
+            printf("%d ", v);
+            for(int w=N;w>0;w--){
+                if(edge[v][w]==TRUE){
+                    if(visit[w]==FALSE){
+                        push(w);
                     }
                 }
             }
         }
     }
-    if(tomato==0){
-        return answer;
-    }
-    else{
-        return -1;
-    }
+}
+
+void reset(){
 }
 
 int main(){
     get_input();
-    printf("%d", bfs());
-    return 0;
+    dfs();
+
 }
+
+
+
