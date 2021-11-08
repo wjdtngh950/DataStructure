ccba83baeea28b9b16cb2149c96f5220cf2d011c
wjdtngh950 <wjdtngh950@naver.com>
2021-11-08 18:01:39 +0900

    Update practice_C++.cpp

--- practice_C++.cpp
+++ practice_C++.cpp
@@ -1,6 +1,5 @@
-#include <iostream>
+#include<iostream>
 #include<stdio.h>
-#include<stdlib.h>
 
 using namespace std;
 
@@ -11,90 +10,69 @@ struct Node{
 };
 
 Node* head;
-Node* last;.
+Node* last;
 Node* cursor;
 
 void addNode(char value){
-    if(head==NULL){
-        last = head = new Node;
+    if(head==cursor){
+        head = new Node;
         head->val=value;
         head->prev=NULL;
-        head->next=NULL;
+        head->next=cursor;
+        cursor->prev=head;
     }
     else{
-        Node* newNode= new Node;
-        newNode->val=value;
-        newNode->prev=last;
-        newNode->next=NULL;
-        last->next=newNode;
-        last=newNode;
+        Node* newNode = new Node;
+        newNode->val = value;
+        newNode->prev=cursor->prev;
+        newNode->next=cursor;
+        cursor->prev->next=newNode;
+        cursor->prev=newNode;
     }
-}
-
-void get_input(){
-    char buf[1100000];
-    //scanf("%s", buf);
-    cin >> buf;
-    for(int i=0;buf[i]!='\0';i++){
-        addNode(buf[i]);
-    }
-    addNode('$');
 }
 
 int main(){
-    get_input();
-    cursor=last;
     int N;
-    //scanf("%d", &N);
-    cin >> N;
-    for(int i=0;i<N;i++){
-        char c;
-        //scanf(" %c", &c);
-        cin >> c;
-        if(c=='L'){
-            if(cursor->prev!=NULL){ // (cursor!=head)
-                cursor=cursor->prev;
-            }
-        }
-        else if(c=='D'){
-            if(cursor->next!=NULL){ //(cursor!=last)
-                cursor=cursor->next;
-            }
-        }
-        else if(c=='B'){
-            if(cursor!=head){
-                if(cursor->prev!=head){
-                    cursor->prev=cursor->prev->prev;
-                    cursor->prev->next=cursor;
-                }
-                else{
-                    head=cursor;
-                    cursor->prev=NULL;
+    cin>>N;
+    for(int n=0;n<N;n++){
+        char buf[1100000];
+        cin>>buf;
+        cursor=new Node;
+        cursor->val = '$';
+        cursor->prev=NULL;
+        cursor->next=NULL;
+        head=last=cursor;
+        for(int i=0;i<buf[i]!='\0';i++){
+            if(buf[i]=='<'){
+                if(cursor!=head){
+                    cursor=cursor->prev;
                 }
             }
-        }
-        else{
-            char c2;
-            scanf(" %c", &c2);
-            cin >> c2;
-            Node* newNode = new Node;
-            newNode->val=c2;
-            newNode->prev=cursor->prev;
-            newNode->next=cursor;
-            if(head==cursor){
-                head=newNode;
+            else if(buf[i]=='>'){
+                if(cursor!=last){
+                    cursor=cursor->next;
+                }
             }
-            else{
-                cursor->prev->next=newNode;
+            else if(buf[i]=='-'){
+                if(cursor!=head){
+                    if(cursor->prev!=head){
+                        cursor->prev->prev->next=cursor;
+                        cursor->prev=cursor->prev->prev;
+                    }
+                    else{
+                        head=cursor;
+                        cursor->prev=NULL;
+                    }
+                }
             }
-            cursor->prev=newNode;
+            else {
+                addNode(buf[i]);
+            }
         }
+        for (Node* p = head; p->val != '$'; p = p->next) {
+            printf("%c", p->val);
+        }
+        printf("\n");
     }
-    for(Node* p=head;p->val!='$';p=p->next){
-        //printf("%c", p->val);
-        cout << p->val;
-    }
-    //printf("\n");
-    cout << endl;
     return 0;
 }
