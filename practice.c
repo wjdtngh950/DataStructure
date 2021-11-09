77173fc7d61d600c57642aa97fe2db5394b6ff8f
wjdtngh950 <wjdtngh950@naver.com>
2021-11-09 23:28:13 +0900

    Update practice.c

--- practice.c
+++ practice.c
@@ -7,13 +7,14 @@
 int front = -1;
 int rear = -1;
 int queue[SIZE][3];
-int dx[8] = { -2, -1,  1,  2,  2,  1, -1, -2 }; //나이트의 이동범위만 적어주면됨 한번에 이동할 수 있는 거리
-int dy[8] = {  1,  2,  2,  1, -1, -2, -2, -1 };
-int visit[310][310];
-int T, N, m, M, l, L;
-int knight = 0;
+int dx[4] = { -1,1,0,0 };
+int dy[4] = { 0,0,-1,1 };
+int N, M;
+int map[101][101];
+int visit[101][101];
+int cnt;
 
-void enqueue(int x, int y, int d) {
+void enqueue(int x, int y, int d) { //d는 cd하기위해서(카톡에 사진) 저장해야하므로 토마토처럼 한번 더
     rear++;
     queue[rear][0] = x;
     queue[rear][1] = y;
@@ -28,10 +29,14 @@ void dequeue(int* x, int* y, int* d) {
 }
 
 void get_input() {
-    scanf("%d", &N);
-    scanf("%d %d", &m, &M);
-    scanf("%d %d", &l, &L);
-    enqueue(m, M, 0); //나이트의 처음갯수는 0
+    scanf("%d %d", &N, &M);
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            char c;
+            scanf(" %c", &c);
+            map[i][j] = c - '0';
+        }
+    }
 }
 
 void bfs() {
@@ -40,14 +45,16 @@ void bfs() {
         dequeue(&cx, &cy, &cd);
         if (visit[cx][cy] == 0) {
             visit[cx][cy] = 1;
-            knight = cd;
-            if (cx == l && cy == L) break;
-            for (int k = 0; k < 8; k++) {
+            cnt = cd;
+            if (cx == N - 1 && cy == M - 1) break;
+            for (int k = 0; k < 4; k++) {
                 int nx = cx + dx[k];
                 int ny = cy + dy[k];
-                if (0 <= nx && nx < N && 0 <= ny && ny < N) {
-                    if (visit[nx][ny] == 0) {
-                        enqueue(nx, ny, cd + 1);
+                if (0 <= nx && nx < N && 0 <= ny && ny < M) {
+                    if (map[nx][ny] == 1) {
+                        if (visit[nx][ny] == 0) {
+                            enqueue(nx, ny, cd + 1);
+                        }
                     }
                 }
             }
@@ -55,24 +62,11 @@ void bfs() {
     }
 }
 
-void init() {
-    front = rear = -1;
-    for (int i = 0; i < 300; i++) {
-        for (int j = 0; j < 300; j++) {
-            visit[i][j] = 0;
-        }
-    }
-}
-
 int main() {
-    scanf("%d", &T);
+    get_input();
+    enqueue(0, 0, 1); // 0,0에서 시작하고, d는 자기자신도 포함하니까 1
+    bfs();
 
-    for (int tc = 0; tc < T; tc++) {
-        init();
-        get_input();
-        bfs();
-        printf("%d\n", knight);
-    }
-
+    printf("%d", cnt);
     return 0;
 }
