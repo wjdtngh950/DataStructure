f2733f8441f7bad33e8d306fe6912d2497744e05
wjdtngh950 <wjdtngh950@naver.com>
2021-11-10 17:53:55 +0900

    Update practice_C++.cpp

--- practice_C++.cpp
+++ practice_C++.cpp
@@ -1,24 +1,25 @@
-#include<iostream>
-#include<stdio.h>
+#include <iostream>
+#include <stdio.h>
+
 using namespace std;
+
 int rear = -1;
 int front = -1;
 int queue[5500000][2];
+int N, K;
+int visit[100001];
 int answer;
-int N,K;
-bool visit[100001];
 
 void get_input(){
     scanf("%d %d", &N, &K);
 }
 
-
 int Next(int x, int idx){
     switch (idx) {
-        case 0 : {
+        case 0: {
             return x-1;
         }
-        case 1 : {
+        case 1: {
             return x+1;
         }
         case 2:{
@@ -28,20 +29,19 @@ int Next(int x, int idx){
 }
 
 int bfs(){
-    while (front!=rear){
-
-        front++;
+    while (front!=rear) {
+        front ++;
         int v=queue[front][0];
         int t=queue[front][1];
         if(v==K){
             return t;
         }
-        if(visit[v]==false){
+        if(!visit[v]){
             visit[v]=true;
             for(int k=0;k<3;k++){
                 int nx=Next(v, k);
                 if(0<=nx&&nx<=100000){
-                    if(visit[nx]== false){
+                    if(!visit[nx]){
                         rear++;
                         queue[rear][0]=nx;
                         queue[rear][1]=t+1;
@@ -57,7 +57,9 @@ int main(){
     rear++;
     queue[rear][0]=N;
     queue[rear][1]=0;
-    answer = bfs();
-   cout << answer;
+
+    answer=bfs();
+    cout<<answer;
+
     return 0;
 }
