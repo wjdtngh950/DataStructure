308a718202119d3f2aa2fe4d2c1af67a7469fc3c
wjdtngh950 <wjdtngh950@naver.com>
2021-11-11 22:36:08 +0900

    Update practice.c

--- practice.c
+++ practice.c
@@ -1,72 +1,63 @@
-#include <stdio.h>
+#include<stdio.h>
 
 #define TRUE 1
 #define FALSE 0
-#define SIZE 5500000
+#define STACK_SIZE 1100000
 
-int front = -1;
-int rear = -1;
-int queue[SIZE][3];
-int dx[4] = { -1,1,0,0 };
-int dy[4] = { 0,0,-1,1 };
-int N, M;
-int map[101][101];
-int visit[101][101];
-int cnt;
+int stack[STACK_SIZE];
+int top=-1;
 
-void enqueue(int x, int y, int d) { //d는 cd하기위해서(카톡에 사진) 저장해야하므로 토마토처럼 한번 더
-    rear++;
-    queue[rear][0] = x;
-    queue[rear][1] = y;
-    queue[rear][2] = d;
+int is_full(){
+    if(top==STACK_SIZE-1){
+        return TRUE;
+    }
+    return FALSE;
 }
 
-void dequeue(int* x, int* y, int* d) {
-    front++;
-    *x = queue[front][0];
-    *y = queue[front][1];
-    *d = queue[front][2];
+int is_empty(){
+    if(top==-1){
+        return TRUE;
+    }
+    return FALSE;
 }
 
-void get_input() {
-    scanf("%d %d", &N, &M);
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            char c;
-            scanf(" %c", &c);
-            map[i][j] = c - '0';
+void push(int val){
+    if(is_full()){
+        return;
+    }
+    top++;
+    stack[top]=val;
+}
+
+int pop(){
+    if(is_empty()){
+        return -1;
+    }
+    int rtn= stack[top];
+    top--;
+    return rtn;
+    //return stack[top--];
+}
+
+int main(){
+    int N;
+    scanf("%d", &N);
+    int sum=0;
+    for(int n=0;n<N;n++){
+        int m;
+        scanf("%d", &m);
+        if(m==0){
+            pop();
+        }
+        else{
+            push(m);
         }
     }
-}
-
-void bfs() {
-    while (front != rear) {
-        int cx, cy, cd;
-        dequeue(&cx, &cy, &cd);
-        if (visit[cx][cy] == 0) {
-            visit[cx][cy] = 1;
-            cnt = cd;
-            if (cx == N - 1 && cy == M - 1) break;
-            for (int k = 0; k < 4; k++) {
-                int nx = cx + dx[k];
-                int ny = cy + dy[k];
-                if (0 <= nx && nx < N && 0 <= ny && ny < M) {
-                    if (map[nx][ny] == 1) {
-                        if (visit[nx][ny] == 0) {
-                            enqueue(nx, ny, cd + 1);
-                        }
-                    }
-                }
-            }
-        }
+    for(int i=0; i<=top;i++){
+        sum+=stack[i];
     }
-}
-
-int main() {
-    get_input();
-    enqueue(0, 0, 1); // 0,0에서 시작하고, d는 자기자신도 포함하니까 1
-    bfs();
-
-    printf("%d", cnt);
+    printf("%d", sum);
     return 0;
+
 }
+
