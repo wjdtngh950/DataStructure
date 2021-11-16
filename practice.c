ed874cf20d6fb9be1ff3b2284e490b7cf3410738
wjdtngh950 <wjdtngh950@naver.com>
2021-11-16 17:20:17 +0900

    Update practice.c

--- practice.c
+++ practice.c
@@ -1,79 +1,45 @@
 #include<stdio.h>
-#include<string.h>
 
-#define TRUE 1
-#define FALSE 0
-#define STACK_SIZE 110000
+#define SIZE 550000
 
-int stack[STACK_SIZE];
 int top = -1;
-int cnt;
+int stack[SIZE];
+int stack2[SIZE]; //기호저장
+int top2 = -1;
+int arr[SIZE];
 
-int is_full() {
-    if (top == STACK_SIZE - 1) {
-        return TRUE;
-    }
-    return FALSE;
-}
-
-int is_empty() {
-    if (top == -1) {
-        return TRUE;
-    }
-    return FALSE;
-}
-
-void push(int val) {
-    if (is_full()) {
-        return;
-    }
-    top++;
-    stack[top] = val;
-}
-
-int pop() {
-    if (is_empty()) {
-        return -1;
-    }
-    int rtn = stack[top];
-    top--;
-    return rtn;
-    //return stack[top--];
-}
 
 int main() {
     int N;
-    int m;
     scanf("%d", &N);
-    char buf[6]; //  가장 긴게 엠티니까
     for (int i = 0; i < N; i++) {
-        scanf("%s", buf);
-        if (buf[0] == 'p' && buf[1] == 'u') {
-            scanf("%d", &m);
-            push(m);
+        scanf("%d", &arr[i]);
+    }
+    int n = 1;
+    int i= 0;
+    while(i<N) {
+        if (top > -1 && stack[top] == arr[i]) {
+            top--;
+            i++;
+            stack2[++top2] = '-';
         }
-        else if(buf[0]=='p'&&buf[1]=='o'){
-            printf("%d\n", pop());
-        }
-        else if(buf[0]=='s'){
-            printf("%d\n", top+1);
-        }
-        else if(buf[0]=='e'){
-            if(is_empty()){
-                printf("%d\n", TRUE);
-            }
-            else{
-                printf("%d\n", FALSE);
+        else if (n<=arr[i]){
+            while (n<=arr[i]) {
+                stack[++top]=n;
+                n++;
+                stack2[++top2] = '+';
             }
         }
         else{
-            if(!is_empty()){
-                printf("%d\n", stack[top]);
-            }
-            else{
-                printf("-1\n");
-            }
+            printf("NO");
+            return 0;
         }
     }
+
+    for(int i=0;i<=top2;i++){
+        printf("%c\n", stack2[i]);
+    }
     return 0;
+
 }
+
