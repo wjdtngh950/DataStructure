96a49f016c33a793a712fe5002b1b110d4e62fdf
wjdtngh950 <wjdtngh950@naver.com>
2021-11-16 23:14:50 +0900

    Create baek_queue_1021.cpp

---
+++ baek_queue_1021.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+
+int rear = -1;
+int front = -1;
+int queue[100000];
+int arr[100000];
+int cnt;
+int N,M;
+
+void enqueue(int val){
+    queue[++rear]=val;
+}
+
+int dequeue(){
+    return queue[++front];
+}
+
+
+void get_input(){
+    int n=1;
+    scanf("%d %d", &N, &M);
+    for(int i=1;i<=N;i++){
+        enqueue(n);
+        n++;
+    }
+    for(int m=0;m<M;m++){
+        scanf("%d", &arr[m]);
+    }
+}
+
+int main(){
+    get_input();
+
+
+    return 0;
+}
