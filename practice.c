b1eda02a9d804fd658bd57e3cef254f40f1e31e5
wjdtngh950 <wjdtngh950@naver.com>
2021-11-16 23:03:14 +0900

    Update practice.c

--- practice.c
+++ practice.c
@@ -1,46 +1,51 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int top = -1;
-int stack[100001];
 
-int top2=-1;
-int stack2[1000001];
+int rear = -1;
+int front = -1;
+int queue[100000];
+int arr[100000];
+int cnt;
+int N,M;
 
-int arr[100001];
-int N;
+void enqueue(int val){
+    queue[++rear]=val;
+}
 
-void get_input() {
-    scanf("%d", &N);
+int dequeue(){
+    return queue[++front];
+}
+
+
+void get_input(){
+    int n=1;
+    scanf("%d %d", &N, &M);
     for(int i=0;i<N;i++){
-        scanf("%d", &arr[i]);
+        rear++;
+        queue[i]=n;
+        n++;
+    }
+    for(int m=1;m<=M;m++){
+        scanf("%d", &arr[m]);
     }
 }
 
 int main(){
     get_input();
-    int n=1;
+    int x=0;
     int i=0;
-    while (i<N){
-        if(n<=arr[i]){
-            while (n<=arr[i]){ //이 부분이 이해가안감
-                stack[++top]=n; //push
-                n++;
-                stack2[++top2]='+';
-            }
-        }
-        else if(top>-1&&stack[top]==arr[i]){
-            top--;
-            i++;
-            stack2[++top2]='-';
+    while (front!=rear){
+        if(queue[i]==arr[x]){
+            dequeue();
+            x++;
         }
         else{
-            printf("NO");
-            return 0;
+            dequeue();
+            enqueue(i);
+            cnt++;
+            i++;
         }
     }
-    for(int t=0;t<=top2;t++){
-        printf("%c\n", stack2[t]);
-    }
+    printf("%d", cnt);
     return 0;
-
 }
