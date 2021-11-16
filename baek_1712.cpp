1ae9afe3e48d9a86a10c1d91141da31b65546e58
wjdtngh950 <wjdtngh950@naver.com>
2021-11-16 23:52:47 +0900

    Update baek_1712.cpp

--- baek_1712.cpp
+++ baek_1712.cpp
@@ -1,4 +1,22 @@
-//
-// Created by 정수호 on 16/11/2021.
-//
+#include<stdio.h>
 
+int A, B, C;
+int BC;
+int AC;
+int answer;
+
+
+int main(){
+    scanf("%d %d %d", &A, &B, &C);
+    AC=A-C;
+    BC=C-B;
+    answer=A/BC;
+    answer++;
+
+    if(AC==B){
+        answer=-1;
+    }
+
+    printf("%d", answer);
+    return 0;
+}
