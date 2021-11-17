54a3ba9024dbdae13e7e978c3b50d84be787c99e
wjdtngh950 <wjdtngh950@naver.com>
2021-11-17 18:26:19 +0900

    Update baek_1712.cpp

--- baek_1712.cpp
+++ baek_1712.cpp
@@ -2,25 +2,20 @@
 
 int A, B, C;
 int BC;
-int answer;
+long long int answer;
 
 
 int main(){
     scanf("%d %d %d", &A, &B, &C);
 
     BC=C-B;
-    if(BC<0){
+    if(BC<=0){
         answer=-1;
     }
     else {
-        if(A==0){
-            answer = 1;
-        }
-        else {
             answer = A / BC;
             answer++;
         }
-    }
-    printf("%d", answer);
+    printf("%lld", answer);
     return 0;
 }
