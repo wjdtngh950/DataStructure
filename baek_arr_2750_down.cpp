ca2cf71475f3d709bf3ffabacca40ec402c6d676
wjdtngh950 <wjdtngh950@naver.com>
2021-11-19 23:35:57 +0900

    Update baek_arr_2750_down.cpp

--- baek_arr_2750_down.cpp
+++ baek_arr_2750_down.cpp
@@ -1,4 +1,32 @@
-//
-// Created by 정수호 on 19/11/2021.
-//
+#include<stdio.h>
 
+int N;
+int arr[1001];
+
+void get_input(){
+    scanf("%d", &N);
+    for(int i=0; i< N;i++){
+        scanf("%d", &arr[i]);
+    }
+}
+
+void down(){
+    for(int i =0; i<N;i++){
+        for(int j=0;j<N-1;j++){
+            if(arr[j]<arr[j+1]){
+                int target=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=target;
+            }
+        }
+    }
+}
+
+int main(){
+    get_input();
+    down();
+    for(int k=0;k<N;k++){
+        printf("%d\n", arr[k]);
+    }
+    return 0;
+}
