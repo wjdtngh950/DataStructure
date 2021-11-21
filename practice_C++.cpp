aa69f8887fc0daaba84460bc5126077449116ced
wjdtngh950 <wjdtngh950@naver.com>
2021-11-21 13:18:21 +0900

    Update practice_C++.cpp

--- practice_C++.cpp
+++ practice_C++.cpp
@@ -12,6 +12,21 @@ void get_input(){
 
 int up(){
     for(int i=0;i<N;i++){
-        
+        for(int j=0;j<N-1;j++){
+            if(arr[j]>arr[j+1]){
+                int target=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=target;
+            }
+        }
     }
+}
+
+int main(){
+    get_input();
+    up();
+    for(int k=0;k<N;k++){
+        printf("%d\n", arr[k]);
+    }
+    return 0;
 }
