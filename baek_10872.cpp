4cc806dde9fbfbe8526754dbd7536695cd4d9e1c
wjdtngh950 <wjdtngh950@naver.com>
2021-11-26 19:32:44 +0900

    Update baek_10872.cpp

--- baek_10872.cpp
+++ baek_10872.cpp
@@ -1,4 +1,18 @@
-//
-// Created by 정수호 on 26/11/2021.
-//
+#include<stdio.h>
 
+int N;
+int sum =1;
+
+void get_input(){
+    scanf("%d", &N);
+}
+
+int main(){
+    get_input();
+    for(int i=N;i>0;i--){
+        sum*=i;
+    }
+
+    printf("%d", sum);
+    return 0;
+}
