8979f688997338ddec6400f11a7d46b001a91b1f
wjdtngh950 <wjdtngh950@naver.com>
2021-11-27 22:11:45 +0900

    Update practice_C++.cpp

--- practice_C++.cpp
+++ practice_C++.cpp
@@ -1,18 +1,30 @@
 #include<stdio.h>
 
-int N;
-int sum =1;
 
-void get_input(){
-    scanf("%d", &N);
+int A, B, C;
+
+int BC;
+
+int sum;
+
+int get_input(){
+    scanf("%d %d %d", &A, &B, &C);
+    BC=C-B;
+    sum = A/BC;
+    if(sum <=0){
+        printf("%d", -1);
+        return 0;
+    }
 }
 
-int main(){
-    get_input();
-    for(int i=N;i>0;i--){
-        sum*=i;
-    }
 
-    printf("%d", sum);
+
+int main() {
+    get_input();
+    if (A <= BC) {
+        printf("%d", 1);
+    } else if (sum >= 0) {
+        printf("%d", sum+1);
+    }
     return 0;
 }
