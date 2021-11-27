ff38e7f75ca54c15b2a72633f1df46e780b2c09c
wjdtngh950 <wjdtngh950@naver.com>
2021-11-27 22:34:07 +0900

    Update practice_C++.cpp

--- practice_C++.cpp
+++ practice_C++.cpp
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
 int N;
+int sum=1;
 void get_input(){
     scanf("%d", &N);
+}
+
+int main(){
+    get_input();
+    for(int i=N;i>0;i--){
+        sum*=i;
+    }
+    printf("%d", sum);
+    return 0;
 }
