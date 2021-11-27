ff1597c63528adca10f43f931ee73abcad4e28f1
wjdtngh950 <wjdtngh950@naver.com>
2021-11-27 23:32:36 +0900

    Update practice_C++.cpp

--- practice_C++.cpp
+++ practice_C++.cpp
@@ -1,10 +1,11 @@
 #include<stdio.h>
 #define SIZE 5500000
+
 int top =-1;
 int stack[SIZE];
 
 int top2= -1;
-int stack2[SIZE];
+char stack2[SIZE];
 
 int arr[SIZE];
 
@@ -15,4 +16,33 @@ void get_input(){
     for(int i=0;i<N;i++){
         scanf("%d", &arr[i]);
     }
+}
+
+int main(){
+    get_input();
+    int i=0;
+    int n=1;
+
+    while(i<N){
+        if(top>-1&&stack[top]==arr[i]){
+            top--;
+            i++;
+            stack2[++top2]='-';
+        }
+        else if(n<=arr[i]){
+            while (n<=arr[i]){
+               stack[++top]=n;
+                n++;
+                stack2[++top2]='+';
+            }
+        }
+        else{
+            printf("NO");
+            return 0;
+        }
+    }
+    for(int k=0;k<top2;k++){
+        printf("%c\n", stack2[k]);
+    }
+    return 0;
 }
