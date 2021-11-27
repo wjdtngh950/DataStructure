cbadf30de3b317f8c9919c668dcce49de3670c44
wjdtngh950 <wjdtngh950@naver.com>
2021-11-27 23:13:48 +0900

    Update practice_C++.cpp

--- practice_C++.cpp
+++ practice_C++.cpp
@@ -1,1 +1,18 @@
 #include<stdio.h>
+#define SIZE 5500000
+int top =-1;
+int stack[SIZE];
+
+int top2= -1;
+int stack2[SIZE];
+
+int arr[SIZE];
+
+int N;
+
+void get_input(){
+    scanf("%d", &N);
+    for(int i=0;i<N;i++){
+        scanf("%d", &arr[i]);
+    }
+}
