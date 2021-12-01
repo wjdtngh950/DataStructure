5fb83bd3f77119f8104ad8b52691e9cfd23931e2
wjdtngh950 <wjdtngh950@naver.com>
2021-12-02 00:16:34 +0900

    Update practice_C++.cpp

--- practice_C++.cpp
+++ practice_C++.cpp
@@ -1,89 +1,79 @@
 #include<stdio.h>
+#include<string.h>
 
 #define TRUE 1
 #define FALSE 0
-#define SIZE 5500000
+#define STACK_SIZE 110000
 
+int stack[STACK_SIZE];
 int top = -1;
-int stack[SIZE][2];
-int dx[4] = { -1, 1, 0, 0 };
-int dy[4] = { 0, 0, -1, 1 };
-int N, M, K;
-int map[50][50];
-int visit[50][50];
-int worm;
+int cnt;
 
-void push(int x, int y) {
+int is_full() {
+    if (top == STACK_SIZE - 1) {
+        return TRUE;
+    }
+    return FALSE;
+}
+
+int is_empty() {
+    if (top == -1) {
+        return TRUE;
+    }
+    return FALSE;
+}
+
+void push(int val) {
+    if (is_full()) {
+        return;
+    }
     top++;
-    stack[top][0] = x;
-    stack[top][1] = y;
+    stack[top] = val;
 }
 
-void pop(int* x, int* y) {
-    *x = stack[top][0];
-    *y = stack[top][1];
+int pop() {
+    if (is_empty()) {
+        return -1;
+    }
+    int rtn = stack[top];
     top--;
-}
-
-void get_input() {
-    scanf("%d %d %d", &M, &N, &K);
-    for (int k = 0; k < K; k++) {
-        int v1, v2;
-        scanf("%d %d", &v1, &v2);
-        map[v2][v1] = 1;
-    } //map에서 k좌표를 1로바꿔줌
-}
-
-void dfs() {
-    while (top != -1) {
-        int cx, cy;
-        pop(&cx, &cy);
-        if (visit[cx][cy] == FALSE) {
-            visit[cx][cy] = TRUE;
-            for (int k = 0; k < 4; k++) {
-                int nx = cx + dx[k];
-                int ny = cy + dy[k];
-                if (0 <= nx && nx < N && 0 <= ny && ny < M) {
-                    if (map[nx][ny] == 1) {
-                        if (visit[nx][ny] == 0) {
-                            push(nx, ny);
-                        }
-                    }
-                }
-            }
-        }
-    }
-    worm++;
-}
-
-void init() {
-    for (int i = 0; i < 50; i++) {
-        for (int j = 0; j < 50; j++) {
-            map[i][j] = 0;
-            visit[i][j] = 0;
-        }
-    }
-    worm = 0;
+    return rtn;
+    //return stack[top--];
 }
 
 int main() {
-    int T;
-    scanf("%d", &T);
-    for (int test_case = 1; test_case <= T; test_case++) {
-        init();
-        get_input();
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (map[i][j] == 1) {
-                    if (visit[i][j] == FALSE) {
-                        push(i, j);
-                        dfs();
-                    }
-                }
+    int N;
+    int m;
+    scanf("%d", &N);
+    char buf[6]; //  가장 긴게 엠티니까
+    for (int i = 0; i < N; i++) {
+        scanf("%s", buf);
+        if (buf[0] == 'p' && buf[1] == 'u') {
+            scanf("%d", &m);
+            push(m);
+        }
+        else if(buf[0]=='p'&&buf[1]=='o'){
+            printf("%d\n", pop());
+        }
+        else if(buf[0]=='s'){
+            printf("%d\n", top+1);
+        }
+        else if(buf[0]=='e'){
+            if(is_empty()){
+                printf("%d\n", TRUE);
+            }
+            else{
+                printf("%d\n", FALSE);
             }
         }
-        printf("%d\n", worm);
+        else{
+            if(!is_empty()){
+                printf("%d\n", stack[top]);
+            }
+            else{
+                printf("-1\n");
+            }
+        }
     }
     return 0;
 }
