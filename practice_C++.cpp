0c1e0e1b81c6688d7ec61de9aa5bcc110c61e4ad
wjdtngh950 <wjdtngh950@naver.com>
2021-12-04 12:05:23 +0900

    Update practice_C++.cpp

--- practice_C++.cpp
+++ practice_C++.cpp
@@ -1,32 +1,79 @@
 #include<stdio.h>
+#include<string.h>
 
-int N;
-int arr[1001];
+#define TRUE 1
+#define FALSE 0
+#define STACK_SIZE 110000
 
-void get_input(){
-    scanf("%d", &N);
-    for(int i=0; i< N;i++){
-        scanf("%d", &arr[i]);
+int stack[STACK_SIZE];
+int top = -1;
+int cnt;
+
+int is_full() {
+    if (top == STACK_SIZE - 1) {
+        return TRUE;
     }
+    return FALSE;
 }
 
-void down(){
-    for(int i =0; i<N;i++){
-        for(int j=0;j<N-1;j++){
-            if(arr[j]<arr[j+1]){
-                int target=arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]=target;
+int is_empty() {
+    if (top == -1) {
+        return TRUE;
+    }
+    return FALSE;
+}
+
+void push(int val) {
+    if (is_full()) {
+        return;
+    }
+    top++;
+    stack[top] = val;
+}
+
+int pop() {
+    if (is_empty()) {
+        return -1;
+    }
+    int rtn = stack[top];
+    top--;
+    return rtn;
+    //return stack[top--];
+}
+
+int main() {
+    int N;
+    int m;
+    scanf("%d", &N);
+    char buf[6]; //  가장 긴게 엠티니까
+    for (int i = 0; i < N; i++) {
+        scanf("%s", buf);
+        if (buf[0] == 'p' && buf[1] == 'u') {
+            scanf("%d", &m);
+            push(m);
+        }
+        else if(buf[0]=='p'&&buf[1]=='o'){
+            printf("%d\n", pop());
+        }
+        else if(buf[0]=='s'){
+            printf("%d\n", top+1);
+        }
+        else if(buf[0]=='e'){
+            if(is_empty()){
+                printf("%d\n", TRUE);
+            }
+            else{
+                printf("%d\n", FALSE);
             }
         }
-    }
-}
-
-int main(){
-    get_input();
-    down();
-    for(int k=0;k<N;k++){
-        printf("%d\n", arr[k]);
+        else{
+            if(!is_empty()){
+                printf("%d\n", stack[top]);
+            }
+            else{
+                printf("-1\n");
+            }
+        }
     }
     return 0;
 }
