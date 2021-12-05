f11a75296baed026d98d81523ff282c77a4cc43f
wjdtngh950 <wjdtngh950@naver.com>
2021-12-05 11:27:35 +0900

    Update practice_C++.cpp

--- practice_C++.cpp
+++ practice_C++.cpp
@@ -1,65 +1,79 @@
-#include <iostream>
-#include <stdio.h>
+#include<stdio.h>
+#include<string.h>
 
-using namespace std;
+#define TRUE 1
+#define FALSE 0
+#define STACK_SIZE 110000
 
-int rear = -1;
-int front = -1;
-int queue[5500000][2];
-int N, K;
-int visit[100001];
-int answer;
+int stack[STACK_SIZE];
+int top = -1;
+int cnt;
 
-void get_input(){
-    scanf("%d %d", &N, &K);
-}
-
-int Next(int x, int idx){
-    switch (idx) {
-        case 0: {
-            return x-1;
-        }
-        case 1: {
-            return x+1;
-        }
-        case 2:{
-            return 2*x;
-        }
+int is_full() {
+    if (top == STACK_SIZE - 1) {
+        return TRUE;
     }
+    return FALSE;
 }
 
-int bfs(){
-    while (front!=rear) {
-        front ++;
-        int v=queue[front][0];
-        int t=queue[front][1];
-        if(v==K){
-            return t;
+int is_empty() {
+    if (top == -1) {
+        return TRUE;
+    }
+    return FALSE;
+}
+
+void push(int val) {
+    if (is_full()) {
+        return;
+    }
+    top++;
+    stack[top] = val;
+}
+
+int pop() {
+    if (is_empty()) {
+        return -1;
+    }
+    int rtn = stack[top];
+    top--;
+    return rtn;
+    //return stack[top--];
+}
+
+int main() {
+    int N;
+    int m;
+    scanf("%d", &N);
+    char buf[6]; //  가장 긴게 엠티니까
+    for (int i = 0; i < N; i++) {
+        scanf("%s", buf);
+        if (buf[0] == 'p' && buf[1] == 'u') {
+            scanf("%d", &m);
+            push(m);
         }
-        if(!visit[v]){
-            visit[v]=true;
-            for(int k=0;k<3;k++){
-                int nx=Next(v, k);
-                if(0<=nx&&nx<=100000){
-                    if(!visit[nx]){
-                        rear++;
-                        queue[rear][0]=nx;
-                        queue[rear][1]=t+1;
-                    }
-                }
+        else if(buf[0]=='p'&&buf[1]=='o'){
+            printf("%d\n", pop());
+        }
+        else if(buf[0]=='s'){
+            printf("%d\n", top+1);
+        }
+        else if(buf[0]=='e'){
+            if(is_empty()){
+                printf("%d\n", TRUE);
+            }
+            else{
+                printf("%d\n", FALSE);
+            }
+        }
+        else{
+            if(!is_empty()){
+                printf("%d\n", stack[top]);
+            }
+            else{
+                printf("-1\n");
             }
         }
     }
-}
-
-int main(){
-    get_input();
-    rear++;
-    queue[rear][0]=N;
-    queue[rear][1]=0;
-
-    answer=bfs();
-    cout<<answer;
-
     return 0;
 }
