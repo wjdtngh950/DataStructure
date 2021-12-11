77867f5c57a4192beaeb3fd8ddda4de30cf9e6b8
wjdtngh950 <wjdtngh950@naver.com>
2021-12-11 16:22:15 +0900

    Update practice_C++.cpp

--- practice_C++.cpp
+++ practice_C++.cpp
@@ -1,63 +1,21 @@
 #include<stdio.h>
 
-#define TRUE 1
-#define FALSE 0
-#define STACK_SIZE 1100000
+int A, B, C;
+int BC;
+long long int answer;
 
-int stack[STACK_SIZE];
-int top=-1;
-
-int is_full(){
-    if(top==STACK_SIZE-1){
-        return TRUE;
-    }
-    return FALSE;
-}
-
-int is_empty(){
-    if(top==-1){
-        return TRUE;
-    }
-    return FALSE;
-}
-
-void push(int val){
-    if(is_full()){
-        return;
-    }
-    top++;
-    stack[top]=val;
-}
-
-int pop(){
-    if(is_empty()){
-        return -1;
-    }
-    int rtn= stack[top];
-    top--;
-    return rtn;
-    //return stack[top--];
-}
 
 int main(){
-    int N;
-    scanf("%d", &N);
-    int sum=0;
-    for(int n=0;n<N;n++){
-        int m;
-        scanf("%d", &m);
-        if(m==0){
-            pop();
-        }
-        else{
-            push(m);
-        }
+    scanf("%d %d %d", &A, &B, &C);
+
+    BC=C-B;
+    if(BC<=0){
+        answer=-1;
     }
-    for(int i=0; i<=top;i++){
-        sum+=stack[i];
+    else {
+        answer = A / BC;
+        answer++;
     }
-    printf("%d", sum);
+    printf("%lld", answer);
     return 0;
-
 }
-
