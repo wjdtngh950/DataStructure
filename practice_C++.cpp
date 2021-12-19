7d005013bdcc266971bce9508a1cce5bb73d22d1
wjdtngh950 <wjdtngh950@naver.com>
2021-12-19 12:59:19 +0900

    Update practice_C++.cpp

--- practice_C++.cpp
+++ practice_C++.cpp
@@ -2,90 +2,62 @@
 
 #define TRUE 1
 #define FALSE 0
-#define SIZE 5500000
+#define STACK_SIZE 1100000
 
-int N, M, K;
-int queue[SIZE][2];
-int front = -1;
-int rear = -1;
-int worm;
+int stack[STACK_SIZE];
+int top=-1;
 
-int map[300][300];
-int visit [300][300];
-
-int dx[4]={-1,1,0,0};
-int dy[4]={0,0,-1,1};
-
-void enqueue(int x, int y){
-    rear++;
-    queue[rear][0]=x;
-    queue[rear][1]=y;
-}
-
-void dequeue(int*x, int*y){
-    front++;
-    *x=queue[front][0];
-    *y=queue[front][1];
-}
-
-void get_input(){
-    scanf("%d %d %d", &M, &N, &K);
-    for(int i=0;i<K;i++) {
-        int v1, v2;
-        scanf("%d %d", &v1, &v2);
-        map[v2][v1]=1;
+int is_full(){
+    if(top==STACK_SIZE-1){
+        return TRUE;
     }
+    return FALSE;
 }
 
-void bfs() {
-    while (rear != front) {
-        int cx, cy;
-        dequeue(&cx, &cy);
-        if (visit[cx][cy] == 0) {
-            visit[cx][cy] = 1;
-            for (int i = 0; i < 4; i++) {
-                int nx = cx + dx[i];
-                int ny = cy + dy[i];
-                if (0 <= nx && nx < N && 0 <= ny && ny < M) {
-                    if (map[nx][ny] == 1) {
-                        if (visit[nx][ny] == 0) {
-                            enqueue(nx, ny);
-                        }
-                    }
-                }
-            }
-        }
+int is_empty(){
+    if(top==-1){
+        return TRUE;
     }
-    worm++;
+    return FALSE;
 }
 
-void resett(){
-    for(int i=0;i<300;i++){
-        for(int j=0;j<300;j++){
-            map[i][j]=0;
-            visit[i][j]=0;
-        }
+void push(int val){
+    if(is_full()){
+        return;
     }
-    worm=0;
+    top++;
+    stack[top]=val;
+}
+
+int pop(){
+    if(is_empty()){
+        return -1;
+    }
+    int rtn= stack[top];
+    top--;
+    return rtn;
+    //return stack[top--];
 }
 
 int main(){
-    int T;
-    scanf("%d", &T);
-    for(int t=0;t<T;t++) {
-        resett();
-        get_input();
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (map[i][j] == 1) {
-                    if (visit[i][j] == 0) {
-                        enqueue(i, j);
-                        bfs();
-                    }
-                }
-            }
+    int N;
+    scanf("%d", &N);
+    int sum=0;
+    for(int n=0;n<N;n++){
+        int m;
+        scanf("%d", &m);
+        if(m==0){
+            pop();
         }
-        printf("%d\n", worm);
+        else{
+            push(m);
+        }
     }
+    for(int i=0; i<=top;i++){
+        sum+=stack[i];
+    }
+    printf("%d", sum);
     return 0;
+
 }
+
