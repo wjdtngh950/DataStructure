8c153aeafe74eb2e72f1f2756affffcb200c4f1e
wjdtngh950 <wjdtngh950@naver.com>
2022-01-06 22:12:22 +0900

    Update practice_C++.cpp

--- practice_C++.cpp
+++ practice_C++.cpp
@@ -1,92 +1,32 @@
-#include<stdio.h> //제출 c11 로 하기
+#include<stdio.h>
 
-#define TRUE 1
-#define FALSE 0
-#define STACK_SIZE 1100000
+int N;
+int arr[1001];
 
-char stack[STACK_SIZE];
-int top = -1;
-
-int is_full() {
-    if (top == STACK_SIZE - 1) {
-        return TRUE;
+void get_input(){
+    scanf("%d", &N);
+    for(int i=0; i<N;i++){
+        scanf("%d", &arr[i]);
     }
-    return FALSE;
 }
 
-int is_empty() {
-    if (top == -1) {
-        return TRUE;
+void up(){
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N-1;j++){
+            if(arr[j]>arr[j+1]){
+                int target=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=target;
+            }
+        }
     }
-    return FALSE;
-}
-
-void push(char val) {
-    if (is_full()) {
-        return;
-    }
-    top++;
-    stack[top] = val;
-}
-
-char pop() {
-    if (is_empty()) {
-        return '\0';
-    }
-    char rtn = stack[top];
-    top--;
-    return rtn;
 }
 
 int main() {
-    while (1) {
-        char buf[102];
-        int error = FALSE;
-        top = -1;
-        gets(buf);
-        // 입력 버퍼
-        if (buf[0] == '.' && buf[1] == '\0') {
-            break;
-        }
-        for (int i = 0; buf[i] != '.'; i++) {
-            if (buf[i] == '(') {
-                push(buf[i]);
-            }
-            else if (buf[i] == ')') {
-                if (is_empty()) {
-                    error = TRUE;
-                    break;
-                }
-                else if (stack[top] != '(') {
-                    error = TRUE;
-                    break;
-                }
-                pop();
-            }
-            else if (buf[i] == '[') {
-                push(buf[i]);
-            }
-            else if (buf[i] == ']') {
-                if (is_empty()) {
-                    error = TRUE;
-                    break;
-                }
-                else if (stack[top] != '[') {
-                    error = TRUE;
-                    break;
-                }
-                pop();
-            }
-        }
-        if (!(is_empty())) {
-            error = TRUE;
-        }
-        if (error == TRUE) {
-            printf("no\n");
-        }
-        else {
-            printf("yes\n");
-        }
+    get_input();
+    up();
+    for (int k = 0; k < N; k++) {
+        printf("%d\n", arr[k]);
     }
     return 0;
 }
