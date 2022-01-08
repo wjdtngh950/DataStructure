909a087d0d9a9ed9f0595344d82f83a47cfbac29
wjdtngh950 <wjdtngh950@naver.com>
2022-01-08 20:53:37 +0900

    Update practice_C++.cpp

--- practice_C++.cpp
+++ practice_C++.cpp
@@ -5,15 +5,15 @@ int arr[1001];
 
 void get_input(){
     scanf("%d", &N);
-    for(int i=0; i<N;i++){
+    for(int i=0; i< N;i++){
         scanf("%d", &arr[i]);
     }
 }
 
-void up(){
-    for(int i=0;i<N;i++){
+void down(){
+    for(int i =0; i<N;i++){
         for(int j=0;j<N-1;j++){
-            if(arr[j]>arr[j+1]){
+            if(arr[j]<arr[j+1]){
                 int target=arr[j];
                 arr[j]=arr[j+1];
                 arr[j+1]=target;
@@ -22,10 +22,10 @@ void up(){
     }
 }
 
-int main() {
+int main(){
     get_input();
-    up();
-    for (int k = 0; k < N; k++) {
+    down();
+    for(int k=0;k<N;k++){
         printf("%d\n", arr[k]);
     }
     return 0;
