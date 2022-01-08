106583d97827bfc0a7300395eeebebb72967db64
wjdtngh950 <wjdtngh950@naver.com>
2022-01-09 00:42:05 +0900

    Update practice_C++.cpp

--- practice_C++.cpp
+++ practice_C++.cpp
@@ -1,32 +1,84 @@
+
 #include<stdio.h>
 
-int N;
-int arr[1001];
+#define TRUE 1
+#define FALSE 0
+#define QUEUE_SIZE 2200000
 
-void get_input(){
-    scanf("%d", &N);
-    for(int i=0; i< N;i++){
-        scanf("%d", &arr[i]);
+int queue[QUEUE_SIZE];
+int front = -1;
+int rear = -1;
+
+int is_full(){
+    if(rear==QUEUE_SIZE-1){
+        return TRUE;
     }
+    return FALSE;
 }
 
-void down(){
-    for(int i =0; i<N;i++){
-        for(int j=0;j<N-1;j++){
-            if(arr[j]<arr[j+1]){
-                int target=arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]=target;
-            }
-        }
+int is_empty(){
+    if(front==rear){
+        return TRUE;
     }
+    return FALSE;
+}
+
+void enqueue(int val){
+    if(is_full()){
+        return;
+    }
+    queue[++rear]=val;
+}
+
+int dequeue(){
+    if(is_empty()){
+        return -1;
+    }
+    return queue[++front];
 }
 
 int main(){
-    get_input();
-    down();
-    for(int k=0;k<N;k++){
-        printf("%d\n", arr[k]);
+    int N;
+    scanf("%d", &N);
+    for(int n=0;n<N;n++){
+        char buf[6];
+        scanf("%s", buf);
+        if(buf[0]=='p'&&buf[1]=='u'){
+            int X;
+            scanf("%d", &X);
+            enqueue(X);
+        }
+        else if(buf[1]=='o'){
+            if(is_empty()){
+                printf("-1\n");
+            }
+            else {
+                printf("%d\n", dequeue());
+            }
+        }
+        else if(buf[0]=='s'){
+            printf("%d\n", rear - front);
+        }
+        else if(buf[0]=='e'){
+            printf("%d\n", is_empty());
+        }
+        else if(buf[0]=='f'){
+            if(is_empty()){
+                printf("-1\n");
+            }
+            else{
+                printf("%d\n", queue[front+1]);
+            }
+        }
+        else{
+            if(is_empty()){
+                printf("-1\n");
+            }
+            else{
+                printf("%d\n",queue[rear]);
+            }
+        }
     }
     return 0;
 }
+
